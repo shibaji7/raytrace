825aeba870dd114f290845051b4fe006c3de6567
Shibaji Chakraborty <shibaji7@vt.edu>
2024-08-06 13:40:06 -0400

    Updated codebase on 08/06 at 13:30 ET

--- rt/density/gemini.py
+++ rt/density/gemini.py
@@ -1,12 +1,12 @@
 
 
-class WAMIPE2d(object):
+
+class GEMINI2d(object):
     def __init__(
         self,
         cfg,
         event,
     ):
         self.cfg = cfg
-        self.file_name = self.cfg.density_file_location
         self.event = event
         return

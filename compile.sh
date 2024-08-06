252a279f62ac895610a86775e89ff2318cd3141f
Shibaji Chakraborty <shibaji7@vt.edu>
2024-08-06 13:37:49 -0400

    Updated codebase on 08/06 at 13:30 ET

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-rm -rf `find -type d -name .ipynb_checkpoints`:
-rm -rf `find -type d -name __pycache__`:
+rm -rf `find -type d -name '.ipynb_checkpoints'`
+rm -rf `find -type d -name '__pycache__'`
 isort -rc -sl .
 autoflake --remove-all-unused-imports -i -r .
 isort -rc -m 3 .
